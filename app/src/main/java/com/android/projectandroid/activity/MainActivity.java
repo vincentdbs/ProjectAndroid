e34bf63eba0bae865d2e81ca53abeb56c4a5cb8f
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2021-02-12 15:39:43 +0100

    constants + async + refactor packages

--- app/src/main/java/com/android/projectandroid/activity/MainActivity.java
+++ app/src/main/java/com/android/projectandroid/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.android.projectandroid;
+package com.android.projectandroid.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.android.projectandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/android/projectandroid/asynctask/AsyncTaskMatch.java
@@ -0,0 +1,13 @@
+package com.android.projectandroid.asynctask;
+
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+public class AsyncTaskMatch extends AsyncTask<String, Void, JSONObject> {
+
+    @Override
+    protected JSONObject doInBackground(String... strings) {
+        return null;
+    }
+}
