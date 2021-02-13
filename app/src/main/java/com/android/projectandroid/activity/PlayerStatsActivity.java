ae9da312e82a9748a52ab2751b71d5f3f8d854c0
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2021-02-13 16:41:24 +0100

    get the id of the player with his name

--- app/src/main/java/com/android/projectandroid/activity/PlayerStatsActivity.java
+++ app/src/main/java/com/android/projectandroid/activity/PlayerStatsActivity.java
@@ -1,18 +1,44 @@
 package com.android.projectandroid.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.projectandroid.R;
+import com.android.projectandroid.asynctask.AsyncTaskMatch;
+import com.android.projectandroid.asynctask.AsyncTaskPlayerStats;
+
+import static com.android.projectandroid.utlis.constants.LOG_TAG;
 
 public class PlayerStatsActivity extends AppCompatActivity {
+    private TextView tvFirstName, tvLastName, tvTeam, tvNumber, tvPoints, tvAssists, tvReboundsO, tvReboundsD, tvSteals, tvBlocks;
+    private SearchView svSearchPlayer;
+    private String playerToSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_stats);
 
+        new AsyncTaskPlayerStats("Lebron", "James").execute("https://www.balldontlie.io/api/v1/players?search=lebron");
 
     }
+
+    private void initFields(){
+        svSearchPlayer = findViewById(R.id.svSearchPlayer);
+        tvFirstName = findViewById(R.id.tv_playerFirstName);
+        tvLastName = findViewById(R.id.tv_playerName);
+        tvPoints = findViewById(R.id.tv_points);
+        tvAssists = findViewById(R.id.tv_assists);
+        tvReboundsO = findViewById(R.id.tv_reboundO);
+        tvReboundsD = findViewById(R.id.tv_reboundD);
+        tvBlocks = findViewById(R.id.tv_blocks);
+        tvSteals = findViewById(R.id.tv_steals);
+        tvTeam = findViewById(R.id.tv_playerTeam);
+        tvNumber = findViewById(R.id.tv_playerNumber);
+    }
+
 }
--- app/src/main/java/com/android/projectandroid/asynctask/AsyncTaskPlayerStats.java
+++ app/src/main/java/com/android/projectandroid/asynctask/AsyncTaskPlayerStats.java
@@ -2,9 +2,11 @@ package com.android.projectandroid.asynctask;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.android.projectandroid.utlis.utils;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -18,16 +20,27 @@ import java.net.URL;
 import static com.android.projectandroid.utlis.constants.LOG_TAG;
 
 public class AsyncTaskPlayerStats extends AsyncTask<String, Void, JSONObject> {
-    @Override
-    protected void onPostExecute(JSONObject jsonObject) {
-        super.onPostExecute(jsonObject);
+    private TextView[] textViews;
+    private String firstName, lastName;
+
+    public AsyncTaskPlayerStats(String firstName, String lastName, TextView... textViews){
+        this.textViews = textViews;
+        this.firstName = firstName.toLowerCase();
+        this.lastName = lastName.toLowerCase();
+    }
+
+    public AsyncTaskPlayerStats(String firstName, String lastName){
+        this.textViews = null;
+        this.firstName = firstName.toLowerCase();
+        this.lastName = lastName.toLowerCase();
     }
 
     @Override
     protected JSONObject doInBackground(String... strings) {
         URL url;
         try {
-            //Replace http by https
+            //Request the api to get all the player that has the same last name that the user want
+            // Cant search by first and last time at the same time
             url = new URL(strings[0]);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
@@ -36,8 +49,7 @@ public class AsyncTaskPlayerStats extends AsyncTask<String, Void, JSONObject> {
                 String s = utils.readStream(in);
 
                 //Get the authenticated value
-                JSONObject result = new JSONObject(s);
-                return result;
+                return new JSONObject(s);
             } catch (JSONException e) {
                 e.printStackTrace();
             } finally {
@@ -49,5 +61,38 @@ public class AsyncTaskPlayerStats extends AsyncTask<String, Void, JSONObject> {
             Log.i(LOG_TAG, e.toString());
         }
         return null;
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject jsonObject) {
+        Log.i(LOG_TAG, jsonObject.toString());
+        try {
+            JSONArray players = jsonObject.getJSONArray("data");
+            //Get the player index by searching by first and last name
+            int jsonObjectIndex = getGoodPlayer(players);
+            if(jsonObjectIndex != -1)  Log.i(LOG_TAG, players.get(jsonObjectIndex).toString());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        super.onPostExecute(jsonObject);
+    }
+
+    private int getGoodPlayer(JSONArray players){
+        try {
+            //Loop through all the found players to find the one with the first and last name wanted
+            for (int i = 0; i < players.length() ; i++) {
+                String fName = players.getJSONObject(i).getString("first_name").toLowerCase();
+                String lName = players.getJSONObject(i).getString("last_name").toLowerCase();
+                if((fName.equals(firstName)) && (lName.equals(lastName))){
+                    //Return the index of the player in the JSONarray
+                    return i;
+                }
+            }
+        }catch (JSONException e) {
+            Log.i(LOG_TAG,e.toString());
+        }
+        // Return -1 if the user is not in the array
+        return -1;
     }
 }
