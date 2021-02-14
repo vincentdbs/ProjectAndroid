845af9d6c260f7a5888f385b8eddcaf8fc4662d1
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2021-02-14 21:05:39 +0100

    link main to stats activity

--- app/src/main/java/com/android/projectandroid/activity/MainActivity.java
+++ app/src/main/java/com/android/projectandroid/activity/MainActivity.java
@@ -5,32 +5,28 @@ import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import android.app.DatePickerDialog;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.projectandroid.R;
 import com.android.projectandroid.adapter.SectionMatchAdapter;
-import com.android.projectandroid.asynctask.AsyncTaskMatch;
 import com.android.projectandroid.fragment.FragmentAllMatch;
 import com.android.projectandroid.fragment.FragmentFavoritesMatch;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.Calendar;
 
-import static com.android.projectandroid.utlis.constants.LOG_TAG;
-
 public class MainActivity extends AppCompatActivity {
 //    TODO faire une classe asynctask mére et heritahe pour celle construite par moi avec overide de la methode on postexecute
     //todo change default date à cote du calendar
     //todo icon n'apparaissent pas dans les tablayout
+    //todo changer logo de l'app
+    //use real toolbar component in main activity
 
-    private ImageView svgCalendar;
+    private ImageView svgCalendar, svgMenu;
     private TextView tv_date;
     private ViewPager vpMatch;
 
@@ -46,16 +42,17 @@ public class MainActivity extends AppCompatActivity {
         svgCalendar = findViewById(R.id.svgCalendar);
         tv_date = findViewById(R.id.date);
         vpMatch = findViewById(R.id.viewPagerMatch);
-        setupViewPager(vpMatch);
+        svgMenu = findViewById(R.id.svgMenu);
 
+        setupViewPager(vpMatch);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.layoutFooter);
         tabLayout.setupWithViewPager(vpMatch);
         tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(getApplication(), R.color.blue));
         setupTabIcons(tabLayout);
         tabSelectedListener(tabLayout);
 
-        addListenerOnClickCalendar();
-
+        addOnClickListenerCalendar();
+        addOnClickListenerMenu();
     }
 
     private void setupTabIcons(TabLayout tabLayout) {
@@ -92,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void addListenerOnClickCalendar(){
+    private void addOnClickListenerCalendar(){
         final Calendar myCalendar = Calendar.getInstance();
 
         DatePickerDialog.OnDateSetListener date = (datePicker, year, month, day) -> {
@@ -109,6 +106,16 @@ public class MainActivity extends AppCompatActivity {
                 myCalendar.get(Calendar.YEAR),
                 myCalendar.get(Calendar.MONTH),
                 myCalendar.get(Calendar.DAY_OF_MONTH)).show());
+    }
+
+    private void addOnClickListenerMenu(){
+        svgMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PlayerStatsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void setupViewPager(ViewPager viewPager) {
