15604e3d2adb2ffd779793ac5972c849790b73e1
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2021-02-14 10:59:46 +0100

    add fragment favorites match

---
+++ app/src/main/java/com/android/projectandroid/fragment/FragmentFavoritesMatch.java
@@ -0,0 +1,19 @@
+package com.android.projectandroid.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.android.projectandroid.R;
+
+public class FragmentFavoritesMatch extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_favorites_match, container, false);
+    }
+}
