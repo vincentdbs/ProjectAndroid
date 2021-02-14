fdf880a40517c44ec7c23b86debbad12e9117ca0
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2021-02-14 21:18:26 +0100

    add favorite team activity

---
+++ app/src/main/java/com/android/projectandroid/activity/FavoriteTeamActivity.java
@@ -0,0 +1,16 @@
+package com.android.projectandroid.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.projectandroid.R;
+
+public class FavoriteTeamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_team);
+    }
+}
