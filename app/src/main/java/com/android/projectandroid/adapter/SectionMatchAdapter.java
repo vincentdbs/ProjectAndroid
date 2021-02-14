f5e04160849ce5cc14010709a929b3f6864e0566
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2021-02-14 11:40:37 +0100

    add pager adapter

---
+++ app/src/main/java/com/android/projectandroid/adapter/SectionMatchAdapter.java
@@ -0,0 +1,44 @@
+package com.android.projectandroid.adapter;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectionMatchAdapter extends FragmentPagerAdapter {
+    // This array list will gonna add the fragment one after another
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    // This list of type string is for the title of the tab
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public static final int BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT = 1;
+
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    public SectionMatchAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return mFragmentList.get(i);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}
