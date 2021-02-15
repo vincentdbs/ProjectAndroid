79b20bf7010f1d0b6cd9807db4c7525273c59460
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2021-02-15 11:49:12 +0100

    sql lite bdd

--- app/src/main/java/com/android/projectandroid/activity/FavoriteTeamActivity.java
+++ app/src/main/java/com/android/projectandroid/activity/FavoriteTeamActivity.java
@@ -3,15 +3,22 @@ package com.android.projectandroid.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.widget.CompoundButton;
 import android.widget.ListView;
 
 import com.android.projectandroid.R;
 import com.android.projectandroid.adapter.TeamAdapter;
+import com.android.projectandroid.database.TeamContract;
+import com.android.projectandroid.database.TeamDbHelper;
 import com.android.projectandroid.model.Team;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static com.android.projectandroid.utlis.constants.MAP_LOGO_TEAM;
 
@@ -20,6 +27,7 @@ public class FavoriteTeamActivity extends AppCompatActivity {
     private SwitchCompat switchFavorite;
     private ArrayList<Team> listOfTeam;
     private TeamAdapter adapter;
+    private TeamDbHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,8 @@ public class FavoriteTeamActivity extends AppCompatActivity {
 
         list = (ListView) findViewById(R.id.lvFavoriteTeam);
         switchFavorite = findViewById(R.id.switchOnlyFavorite);
+
+        dbHelper = new TeamDbHelper(getApplicationContext());
 
 //        todo trouve un autre moyen car pas erreur en fonction de la version
         // todo remplacer par une bdd locale
@@ -41,6 +51,8 @@ public class FavoriteTeamActivity extends AppCompatActivity {
         list.setAdapter(adapter);
 
         onClickSwitch();
+
+        database();
     }
 
     private void onClickSwitch(){
@@ -50,12 +62,16 @@ public class FavoriteTeamActivity extends AppCompatActivity {
                 // true if the switch is in the On position
                 listOfTeam.clear();
                 if(isChecked){
+                    //        todo trouve un autre moyen car pas erreur en fonction de la version
+
                     MAP_LOGO_TEAM.forEach((str, team) -> {
                         if(team.isFavorites()){
                             listOfTeam.add(new Team(team.getLogo(), team.getName(), team.getAbreviation(), team.getCity(), team.isFavorites()));
                         }
                     });
                 }else{
+//        todo trouve un autre moyen car pas erreur en fonction de la version
+
                     MAP_LOGO_TEAM.forEach((str, team) -> {
                         listOfTeam.add(new Team(team.getLogo(), team.getName(), team.getAbreviation(), team.getCity(), team.isFavorites()));
                     });
@@ -63,6 +79,70 @@ public class FavoriteTeamActivity extends AppCompatActivity {
                 adapter.notifyDataSetChanged();
             }
         });
+    }
+
+    public void database(){
+
+
+        // Gets the data repository in write mode
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+// Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(TeamContract.TeamEntry.COLUMN_NAME_FAVORITE, 1);
+        values.put(TeamContract.TeamEntry.COLUMN_NAME_ABREV, "SAS");
+        values.put(TeamContract.TeamEntry.COLUMN_NAME_CITY, "San Antonio");
+        values.put(TeamContract.TeamEntry.COLUMN_NAME_NAME, "Spurs");
+        values.put(TeamContract.TeamEntry.COLUMN_NAME_LOGO, R.drawable.logo_spurs);
+
+// Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(TeamContract.TeamEntry.TABLE_NAME, null, values);
+
+
+
+// Define a projection that specifies which columns from the database
+// you will actually use after this query.
+        String[] projection = {
+                BaseColumns._ID,
+                TeamContract.TeamEntry.COLUMN_NAME_FAVORITE,
+                TeamContract.TeamEntry.COLUMN_NAME_ABREV,
+                TeamContract.TeamEntry.COLUMN_NAME_CITY,
+                TeamContract.TeamEntry.COLUMN_NAME_NAME,
+                TeamContract.TeamEntry.COLUMN_NAME_LOGO
+        };
+
+// Filter results WHERE "title" = 'My Title'
+        String selection = TeamContract.TeamEntry.COLUMN_NAME_ABREV + " = ?";
+        String[] selectionArgs = { "SAS" };
+
+// How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                TeamContract.TeamEntry.COLUMN_NAME_ABREV + " DESC";
+
+        Cursor cursor = db.query(
+                TeamContract.TeamEntry.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
+
+        List itemIds = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            long itemId = cursor.getLong(
+                    cursor.getColumnIndexOrThrow(TeamContract.TeamEntry._ID));
+            itemIds.add(itemId);
+        }
+        cursor.close();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        dbHelper.close();
+        super.onDestroy();
     }
 
 }
---
+++ app/src/main/java/com/android/projectandroid/database/TeamDbHelper.java
@@ -0,0 +1,34 @@
+package com.android.projectandroid.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+
+import androidx.annotation.Nullable;
+
+import static com.android.projectandroid.database.TeamContract.TeamEntry.SQL_CREATE_ENTRIES;
+import static com.android.projectandroid.database.TeamContract.TeamEntry.SQL_DELETE_ENTRIES;
+
+public class TeamDbHelper extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Team.db";
+
+    public TeamDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+}
