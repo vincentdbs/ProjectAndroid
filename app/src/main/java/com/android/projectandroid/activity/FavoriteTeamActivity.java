0a324f770e04447529726d9485016af0188e31fa
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2021-02-15 10:03:00 +0100

    design team cart + map team

--- app/src/main/java/com/android/projectandroid/activity/FavoriteTeamActivity.java
+++ app/src/main/java/com/android/projectandroid/activity/FavoriteTeamActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.android.projectandroid.R;
 import com.android.projectandroid.adapter.MatchListAdapter;
@@ -13,18 +14,21 @@ import com.android.projectandroid.model.Team;
 
 import java.util.ArrayList;
 
+import static com.android.projectandroid.utlis.constants.MAP_LOGO_TEAM;
+
 public class FavoriteTeamActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite_team);
-
         ArrayList<Team> listOfTeam = new ArrayList<>();
-        listOfTeam.add(new Team(R.drawable.logo_76ers, "76ers", "76ers", "Philadelphia", true));
-        listOfTeam.add(new Team(R.drawable.logo_bucks, "76ers", "76ers", "Philadelphia", true));
-        listOfTeam.add(new Team(R.drawable.logo_cavaliers, "76ers", "76ers", "Philadelphia", true));
-        listOfTeam.add(new Team(R.drawable.logo_clippers, "76ers", "76ers", "Philadelphia", true));
+
+//        todo trouve un autre moyen car pas erreur en fonction de la version
+        MAP_LOGO_TEAM.forEach((str, team) -> {
+            listOfTeam.add(new Team(team.getLogo(), team.getName(), team.getAbreviation(), team.getCity(), team.isFavorites()));
+        });
+
 
         TeamAdapter adapter = new TeamAdapter(getApplicationContext(), listOfTeam);
 
